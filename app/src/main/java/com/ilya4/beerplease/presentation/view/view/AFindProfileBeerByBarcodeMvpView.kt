2e95da8cdf3581f952e8458032644381b3905564
ilya4 <ilja.chitneev@gmail.com>
2019-11-27 22:06:34 +0300

    deleted moxy layout

--- app/src/main/java/com/ilya4/beerplease/presentation/view/view/AFindProfileBeerByBarcodeMvpView.kt
+++ app/src/main/java/com/ilya4/beerplease/presentation/view/view/AFindProfileBeerByBarcodeMvpView.kt
@@ -1,6 +1,8 @@
 package com.ilya4.beerplease.presentation.view.view
 
 import moxy.MvpView
+import moxy.viewstate.strategy.AddToEndSingleStrategy
+import moxy.viewstate.strategy.StateStrategyType
 
-interface AFindProfileBeerByBarcodeMvpView: MvpView {
-}
+@StateStrategyType(AddToEndSingleStrategy::class)
+interface AFindProfileBeerByBarcodeMvpView: MvpView
--- app/src/main/java/com/ilya4/beerplease/presentation/view/view/FBeerCardMvpView.kt
+++ app/src/main/java/com/ilya4/beerplease/presentation/view/view/FBeerCardMvpView.kt
@@ -1,5 +1,8 @@
 package com.ilya4.beerplease.presentation.view.view
 
 import moxy.MvpView
+import moxy.viewstate.strategy.AddToEndSingleStrategy
+import moxy.viewstate.strategy.StateStrategyType
 
+@StateStrategyType(AddToEndSingleStrategy::class)
 interface FBeerCardMvpView: MvpView
